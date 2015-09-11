ca927b4a46e434d6c941afa6ba8d7d8c0dff37a7
Cosette24601 <crazycosette@gmail.com>
2015-09-11 23:49:08 +0200

    update

---
+++ Escape/Assets/Scripts/NextScene3.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel("MainScene"); 
+		}
+	}
+}
